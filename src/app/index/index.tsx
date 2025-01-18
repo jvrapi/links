d28a8653a09de4e61d70154be2829f1bbb47beae
João Vitor Lima <joaooviitoorr@gmail.com>
2025-01-17 21:12:17 -0300

    feat: create header

---
+++ src/app/index/index.tsx
@@ -0,0 +1,17 @@
+import { Image, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
+import { MaterialIcons } from '@expo/vector-icons'
+import { colors } from '@/styles/colors'
+
+export default function Index() {
+  return (
+    <View>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+        <TouchableOpacity>
+          <MaterialIcons name='add' size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
